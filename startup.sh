48e04356e5ff5893baec2dfcb6948310d6d46d5d
alanaolivieri <alana.olivieri@gmail.com>
2024-08-05 10:59:21 +0200

    add jupyter lab

--- startup.sh
+++ startup.sh
@@ -17,5 +17,8 @@ fi
 # Start code-server
 code-server --bind-addr 0.0.0.0:8080 --auth none /workspace &
 
+# Start Jupyter Lab
+jupyter lab --ip=0.0.0.0 --port=8888 --no-browser --allow-root &
+
 # Wait for all background processes to finish
 wait
